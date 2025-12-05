be27b8d0ef76863cde2f38cd76097a42388fa781
GigiRighi <giovana.righi@aluno.senai.br>
2025-12-05 16:19:43 -0300

    criando a página de contato

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,34 +24,6 @@ setInterval(function() {
     
 }, 5000);
 
-
-
 exibirImagem()
  indiceAtualListaImagens++
-
-
-document.getElementById("contatoForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  // Pega os valores do formulário
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const telefone = document.getElementById("telefone").value;
-  const comentario = document.getElementById("comentario").value;
-
-  
-  const numeroWhatsApp = "5519991753130"; 
-
-  const mensagem = `Olá! Novo contato via site:%0A
-  Nome: ${nome}%0A
-  E-mail: ${email}%0A
-  Telefone: ${telefone}%0A
-  Comentário: ${comentario}`;
-
-
-  const url = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
-
-  
-  window.open(url, "_blank");
-});
 
